03a26c6191fdae105168cf806b16a64ed05d04de
simon <75259597+tzimom@users.noreply.github.com>
2025-11-19 14:04:56 +0100

    Fix style inconsistency

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,7 +5,7 @@ return {
     ft = "tex",
 
     init = function()
-        vim.g.vimtex_view_method = 'skim'
+        vim.g.vimtex_view_method = "skim"
         vim.g.vimtex_view_skim_activate = 1
         vim.g.vimtex_view_skim_sync = 1
     end,
