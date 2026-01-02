6bdd51355f82a5b65f79837c846f48e12692945a
simon <75259597+tzimom@users.noreply.github.com>
2026-01-02 23:05:32 +0100

    Add snippets for large umlauts

--- snippets/all.lua
+++ snippets/all.lua
@@ -4,7 +4,10 @@ local text_node = luasnip.text_node
 
 return {
     snippet({ trig = "--a", snippetType = "autosnippet", wordTrig = false }, { text_node("ä") }),
+    snippet({ trig = "--A", snippetType = "autosnippet", wordTrig = false }, { text_node("Ä") }),
     snippet({ trig = "--o", snippetType = "autosnippet", wordTrig = false }, { text_node("ö") }),
+    snippet({ trig = "--O", snippetType = "autosnippet", wordTrig = false }, { text_node("Ö") }),
     snippet({ trig = "--u", snippetType = "autosnippet", wordTrig = false }, { text_node("ü") }),
+    snippet({ trig = "--U", snippetType = "autosnippet", wordTrig = false }, { text_node("Ü") }),
     snippet({ trig = "--s", snippetType = "autosnippet", wordTrig = false }, { text_node("ß") }),
 }
