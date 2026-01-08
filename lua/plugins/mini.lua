8f50fe3def6c96165abec29809a87d590937048d
simon <75259597+tzimom@users.noreply.github.com>
2026-01-08 14:53:33 +0100

    Remove mini.clue and change some keybinds

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -16,10 +16,10 @@ return {
             mappings = {
                 add = "gsa",
                 delete = "gsd",
+                replace = "gsr",
                 find = "gsf",
                 find_left = "gsF",
                 highlight = "gsh",
-                replace = "gsr",
             },
         },
     },
@@ -32,79 +32,79 @@ return {
             { "grfT", function() require("mini.trailspace").trailspace.trim_last_lines() end,  desc = "Trim last blank lines" },
         }
     },
-    {
-        "nvim-mini/mini.clue", version = false,
-        event = "VeryLazy",
-        config = function()
-            local miniclue = require("mini.clue")
-
-            miniclue.setup({
-                triggers = {
-                    -- Leader triggers
-                    { mode = "n", keys = "<leader>" },
-                    { mode = "x", keys = "<leader>" },
-                    { mode = "n", keys = "<localleader>" },
-                    { mode = "x", keys = "<localleader>" },
-
-                    -- `g` key
-                    { mode = "n", keys = "g" },
-                    { mode = "x", keys = "g" },
-
-                    -- Marks
-                    { mode = "n", keys = "'" },
-                    { mode = "n", keys = "`" },
-                    { mode = "x", keys = "'" },
-                    { mode = "x", keys = "`" },
-
-                    -- Registers
-                    { mode = "n", keys = "\"" },
-                    { mode = "x", keys = "\"" },
-                    { mode = "i", keys = "<c-r>" },
-                    { mode = "c", keys = "<c-r>" },
-
-                    -- Window commands
-                    { mode = "n", keys = "<c-w>" },
-
-                    -- `z` key
-                    { mode = "n", keys = "z" },
-                    { mode = "x", keys = "z" },
-
-                    { mode = "n", keys = "[" },
-                    { mode = "n", keys = "]" },
-                },
-                clues = {
-                    miniclue.gen_clues.builtin_completion(),
-                    miniclue.gen_clues.g(),
-                    miniclue.gen_clues.marks(),
-                    miniclue.gen_clues.registers(),
-
-                    miniclue.gen_clues.windows({
-                        submode_move = false,
-                        submode_navigate = true,
-                        submode_resize = true,
-                    }),
-
-                    miniclue.gen_clues.z(),
-                    miniclue.gen_clues.square_brackets(),
-
-                    { mode = "n", keys = "<leader>f", desc = "+File" },
-                    { mode = "n", keys = "<leader>b", desc = "+Buffer" },
-                    { mode = "n", keys = "<leader>s", desc = "+Search" },
-                    { mode = "n", keys = "<leader>g", desc = "+Git" },
-                    { mode = "n", keys = "<leader>u", desc = "+UI" },
-                    { mode = "n", keys = "<leader>n", desc = "+Notifications" },
-
-                    { mode = "n", keys = "<leader>c", desc = "+Code" },
-                    { mode = "n", keys = "<leader>cx", desc = "+Trouble" },
-
-                    { mode = "n", keys = "gs", desc = "+Surround" },
-
-                    { mode = "n", keys = "<localleader>l", desc = "+VimTeX" },
-                },
-                window = {
-                    delay = 300,
-                },
-            })
-        end,
-    },
+    -- {
+    --     "nvim-mini/mini.clue", version = false,
+    --     event = "VeryLazy",
+    --     config = function()
+    --         local miniclue = require("mini.clue")
+    --
+    --         miniclue.setup({
+    --             triggers = {
+    --                 -- Leader triggers
+    --                 { mode = "n", keys = "<leader>" },
+    --                 { mode = "x", keys = "<leader>" },
+    --                 { mode = "n", keys = "<localleader>" },
+    --                 { mode = "x", keys = "<localleader>" },
+    --
+    --                 -- `g` key
+    --                 { mode = "n", keys = "g" },
+    --                 { mode = "x", keys = "g" },
+    --
+    --                 -- Marks
+    --                 { mode = "n", keys = "'" },
+    --                 { mode = "n", keys = "`" },
+    --                 { mode = "x", keys = "'" },
+    --                 { mode = "x", keys = "`" },
+    --
+    --                 -- Registers
+    --                 { mode = "n", keys = "\"" },
+    --                 { mode = "x", keys = "\"" },
+    --                 { mode = "i", keys = "<c-r>" },
+    --                 { mode = "c", keys = "<c-r>" },
+    --
+    --                 -- Window commands
+    --                 { mode = "n", keys = "<c-w>" },
+    --
+    --                 -- `z` key
+    --                 { mode = "n", keys = "z" },
+    --                 { mode = "x", keys = "z" },
+    --
+    --                 { mode = "n", keys = "[" },
+    --                 { mode = "n", keys = "]" },
+    --             },
+    --             clues = {
+    --                 miniclue.gen_clues.builtin_completion(),
+    --                 miniclue.gen_clues.g(),
+    --                 miniclue.gen_clues.marks(),
+    --                 miniclue.gen_clues.registers(),
+    --
+    --                 miniclue.gen_clues.windows({
+    --                     submode_move = false,
+    --                     submode_navigate = true,
+    --                     submode_resize = true,
+    --                 }),
+    --
+    --                 miniclue.gen_clues.z(),
+    --                 miniclue.gen_clues.square_brackets(),
+    --
+    --                 { mode = "n", keys = "<leader>b", desc = "+Buffer" },
+    --                 { mode = "n", keys = "<leader>s", desc = "+Search" },
+    --                 { mode = "n", keys = "<leader>g", desc = "+Git" },
+    --                 { mode = "n", keys = "<leader>u", desc = "+UI" },
+    --                 { mode = "n", keys = "<leader>n", desc = "+Notifications" },
+    --                 { mode = "n", keys = "<leader>q", desc = "+Quit Neovim" },
+    --
+    --                 { mode = "n", keys = "<leader>c", desc = "+Code" },
+    --                 { mode = "n", keys = "<leader>cx", desc = "+Trouble" },
+    --
+    --                 { mode = "n", keys = "gs", desc = "+Surround" },
+    --
+    --                 { mode = "n", keys = "<localleader>l", desc = "+VimTeX" },
+    --             },
+    --             window = {
+    --                 delay = 300,
+    --             },
+    --         })
+    --     end,
+    -- },
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,15 +7,17 @@ return {
     keys = {
         { "<leader>bd", function() require("snacks").bufdelete() end, desc = "Delete current buffer" },
 
-        { "<leader>ff", function() require("snacks").picker.files() end, desc = "Find file" },
-        { "<leader>fg", function() require("snacks").picker.git_files() end, desc = "Find git file" },
-
         { "<leader>gg", function() require("snacks").lazygit() end, desc = "Lazy git" },
         { "<leader>gl", function() require("snacks").lazygit.log() end, desc = "Git log" },
         { "<leader>gc", function() require("snacks").picker.git_branches() end, desc = "Git checkout" },
 
-        { "<leader>sg", function() require("snacks").picker.grep() end, desc = "Grep" },
-        { "<leader>sg", function() require("snacks").picker.grep_word() end, desc = "Grep selection", mode = "x" },
+        { "<leader>sf", function() require("snacks").picker.files() end, desc = "Search files" },
+        { "<leader>sg", function() require("snacks").picker.git_files() end, desc = "Search git files" },
+        { "<leader>sG", function() require("snacks").picker.grep() end, desc = "Grep" },
+        { "<leader>sG", function() require("snacks").picker.grep_word() end, desc = "Grep selection", mode = "x" },
+        { "<leader>sb", function() require("snacks").picker.buffers() end, desc = "Search buffers" },
+        { "<leader>sd", function() require("snacks").picker.diagnostics() end, desc = "Search diagnostics" },
+        { "<leader>sD", function() require("snacks").picker.diagnostics_buffer() end, desc = "Search diagnostics (buffer)" },
         { "<leader>sk", function() require("snacks").picker.keymaps() end, desc = "Search keymaps" },
 
         { "<leader>ct", function() require("snacks").terminal() end, desc = "Open terminal" },
