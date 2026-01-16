ab93fa33c59c47fd8f1267d828a5049f23fa7169
simon <75259597+tzimom@users.noreply.github.com>
2026-01-16 12:24:35 +0100

    Configure vtsls to work with vue_ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -14,5 +14,28 @@ return {
         })
 
         vim.lsp.enable("sourcekit")
+
+        local mason_path = vim.fn.stdpath("data") .. "/mason"
+        local vue_plugin_path = mason_path .. "/packages/vue-language-server/node_modules/@vue/language-server"
+
+        vim.lsp.config("vtsls", {
+            filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+            settings = {
+                vtsls = {
+                    tsserver = {
+                        globalPlugins = {
+                            {
+                                name = "@vue/typescript-plugin",
+                                location = vue_plugin_path,
+                                languages = { "vue" },
+                                configNamespace = "typescript",
+                                enableForWorkspaceTypeScriptVersions = true,
+                            },
+                        },
+                    },
+                },
+            },
+        })
+
     end,
 }
