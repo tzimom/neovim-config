cd01ab09bac2a4fcf1b961c8eed24b1d2581a8f0
tzimom <75259597+tzimom@users.noreply.github.com>
2026-02-11 11:26:00 +0100

    Change some oil.nvim settings

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,26 +1,35 @@
 return {
-    "stevearc/oil.nvim",
+	"stevearc/oil.nvim",
 
-    event = "VeryLazy",
-    keys = { { "<leader>o", function() require("oil").toggle_float() end, desc = "Toggle oil" } },
+	event = "VeryLazy",
+	keys = { {
+		"<leader>o",
+		function()
+			require("oil").toggle_float()
+		end,
+		desc = "Toggle oil",
+	} },
 
-    opts = {
-        default_file_explorer = true,
-        columns = {},
-        keymaps = {
-            ["q"] = "actions.close",
+	opts = {
+		default_file_explorer = true,
+		keymaps = { ["q"] = "actions.close" },
+		delete_to_trash = true,
+		view_options = {
+            show_hidden = true,
+            natural_order = true,
         },
-        delete_to_trash = true,
-        view_options = { show_hidden = true },
-        skip_confirm_for_simple_edits = true,
-    },
+		skip_confirm_for_simple_edits = true,
+        win_options = { wrap = true },
+	},
 
-    config = function(_, opts)
-        require("oil").setup(opts)
+	config = function(_, opts)
+		require("oil").setup(opts)
 
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = "oil",
-            callback = function() vim.opt_local.cursorline = true end,
-        })
-    end,
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "oil",
+			callback = function()
+				vim.opt_local.cursorline = true
+			end,
+		})
+	end,
 }
