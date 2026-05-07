5525c5be5d404bf1a4c6b558ae80e57074866e7a
tzimom <75259597+tzimom@users.noreply.github.com>
2026-05-07 12:08:29 +0200

    refactor: change snippets for german letters

--- snippets/all.lua
+++ snippets/all.lua
@@ -3,11 +3,11 @@ local snippet = luasnip.snippet
 local text_node = luasnip.text_node
 
 return {
-    snippet({ trig = "--a", snippetType = "autosnippet", wordTrig = false }, { text_node("ä") }),
-    snippet({ trig = "--A", snippetType = "autosnippet", wordTrig = false }, { text_node("Ä") }),
-    snippet({ trig = "--o", snippetType = "autosnippet", wordTrig = false }, { text_node("ö") }),
-    snippet({ trig = "--O", snippetType = "autosnippet", wordTrig = false }, { text_node("Ö") }),
-    snippet({ trig = "--u", snippetType = "autosnippet", wordTrig = false }, { text_node("ü") }),
-    snippet({ trig = "--U", snippetType = "autosnippet", wordTrig = false }, { text_node("Ü") }),
-    snippet({ trig = "--s", snippetType = "autosnippet", wordTrig = false }, { text_node("ß") }),
+    snippet({ trig = "aaa", snippetType = "autosnippet", wordTrig = false }, { text_node("ä") }),
+    snippet({ trig = "ooo", snippetType = "autosnippet", wordTrig = false }, { text_node("ö") }),
+    snippet({ trig = "uuu", snippetType = "autosnippet", wordTrig = false }, { text_node("ü") }),
+    snippet({ trig = "AAA", snippetType = "autosnippet", wordTrig = false }, { text_node("Ä") }),
+    snippet({ trig = "OOO", snippetType = "autosnippet", wordTrig = false }, { text_node("Ö") }),
+    snippet({ trig = "UUU", snippetType = "autosnippet", wordTrig = false }, { text_node("Ü") }),
+    snippet({ trig = "sss", snippetType = "autosnippet", wordTrig = false }, { text_node("ß") }),
 }
