00c736382ad22476a27358aaed098ccd21c5c339
tzimom <75259597+tzimom@users.noreply.github.com>
2026-05-07 12:08:54 +0200

    feat: sync ]b and [b with bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,5 +4,9 @@ return {
     event = "VeryLazy",
     dependencies = "nvim-tree/nvim-web-devicons",
 
+    keys = {
+        { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+        { "[b", "<Cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer" },
+    },
     opts = {},
 }
