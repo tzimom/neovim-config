5071da4119fb781c799241f938f5653da9093a2f
tzimom <75259597+tzimom@users.noreply.github.com>
2026-05-07 12:12:16 +0200

    feat: add ]<a-b> and [<a-b> for moving buffers

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,6 +7,8 @@ return {
     keys = {
         { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
         { "[b", "<Cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer" },
+        { "]<a-b>", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer right" },
+        { "[<a-b>", "<Cmd>BufferLineMovePrev<cr>", desc = "Move buffer left" },
     },
     opts = {},
 }
